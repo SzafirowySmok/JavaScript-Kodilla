a68fb913dd429735ba87ef35d434c24061be4cd3
ASP <dragon.ka@wp.pl>
2018-01-20 16:14:04 +0100

    Zadanie 5.7

---
+++ Zad5.7/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID':'515704032134-0oucnmibe8mmb2d07cotgl59rf0avgel.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': 'A1nUvLqK1-9XFWX-Z4T3eQWz',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
