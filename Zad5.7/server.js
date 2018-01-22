2f08509b092ef9377c5ad9026cda8bb095a8cec2
ASP <dragon.ka@wp.pl>
2018-01-22 21:12:51 +0100

    poprawki

--- Zad5.7/server.js
+++ Zad5.7/server.js
@@ -45,7 +45,7 @@ app.get('/auth/google',
     passport.authenticate('google', {
         scope: ['profile', 'email']
 }));
-app.get('/auth/google/callack',
+app.get('/auth/google/callback',
     passport.authenticate('google', {
         successRedirect: '/logged',
         failtureRedirect: '/'
