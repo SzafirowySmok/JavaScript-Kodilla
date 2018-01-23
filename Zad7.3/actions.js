856bc0b6e744a7c17c6fbba6a19fc1587bfd1e90
ASP <dragon.ka@wp.pl>
2018-01-23 21:43:55 +0100

    poprawka

--- Zad7.3/actions.js
+++ Zad7.3/actions.js
@@ -1,15 +1,16 @@
 import uuid from 'uuid';
 
-const ADD_COMMENT = 'ADD_COMMENT';
-const EDIT_COMMENT = 'EDIT_COMMENT';
-const REMOVE_COMMENT = 'REMOVE_COMMENT';
-const THUMB_UP_COMMENT = 'THUMB_UP_COMMENT';
-const THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT';
+export const ADD_COMMENT = 'ADD_COMMENT';
+export const EDIT_COMMENT = 'EDIT_COMMENT';
+export const REMOVE_COMMENT = 'REMOVE_COMMENT';
+export const THUMB_UP_COMMENT = 'THUMB_UP_COMMENT';
+export const THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT';
 
 function addComment(text) {
     return {
         type: ADD_COMMENT,
         text,
+        votes: 0,
         id: uuid.v4()
     }
 }
@@ -17,6 +18,7 @@ function editComment(text, id) {
     return {
         type: EDIT_COMMENT,
         text,
+        votes,
         id
     }
 }
